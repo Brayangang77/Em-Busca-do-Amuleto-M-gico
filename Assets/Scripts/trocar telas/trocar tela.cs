900f4d27a9624255939325299c5082dfe26c392f
Brayangang77 <brayan_b_rodrigues@estudante.sesisenai.org.br>
2026-07-16 17:12:54 -0300

    Initial commit

---
+++ Assets/Scripts/trocar telas/trocar tela.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TrocarTela : MonoBehaviour
+{
+    public void IrParaCena2()
+    {
+        SceneManager.LoadScene("Cena2");
+    }
+}
